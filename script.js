d551220cfa8f3752d114a456fb72bd8ca69e8b22
Shomik Das <dashomik@gamil.com>
2023-09-10 09:21:48 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,50 @@
+ const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signupBtn = document.querySelector("#signup"),
+  loginBtn = document.querySelector("#login"),
+  forgotBtn = document.querySelector("#forgot_password"),
+  loginnewBtn = document.querySelector("#loginnew"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
+
+
+
+formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
+
+
+
+
+pwShowHide.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let getPwInput = icon.parentElement.querySelector("input");
+    if (getPwInput.type === "password") {
+      getPwInput.type = "text";
+      icon.classList.replace("uil-eye-slash", "uil-eye");
+    } else {
+      getPwInput.type = "password";
+      icon.classList.replace("uil-eye", "uil-eye-slash");
+    }
+  });
+});
+
+
+
+signupBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("active");
+});
+loginBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("active");
+});
+
+forgotBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.add("activeForgot");
+});
+loginnewBtn.addEventListener("click", (e) => {
+  e.preventDefault();
+  formContainer.classList.remove("activeForgot");
+});
